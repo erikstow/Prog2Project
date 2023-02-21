e377d40bd45c736286b74502216837efe54a7e05
Torgeir Haukaas <toha12744@gmail.com>
2023-02-21 14:22:36 +0100

    Add tests to Link and Passage classes

--- src/test/java/edu/ntnu/idatt2001/LinkTest.java
+++ src/test/java/edu/ntnu/idatt2001/LinkTest.java
@@ -3,14 +3,13 @@ package edu.ntnu.idatt2001;
 import edu.ntnu.idatt2001.actions.GoldAction;
 import edu.ntnu.idatt2001.actions.Action;
 import edu.ntnu.idatt2001.actions.ScoreAction;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LinkTest {
   public Link link;
@@ -20,40 +19,60 @@ class LinkTest {
     link = new Link("Text", "Reference");
   }
 
-  @Test
-  void getReference() {
-    assertEquals("Reference", link.getReference());
+  @Nested
+  class LinkGetters {
+    @Test
+    void getReference() {
+      assertEquals("Reference", link.getReference());
+    }
+
+    @Test
+    void getActionsWithTwoActions() {
+      GoldAction goldAction = new GoldAction(100);
+      ScoreAction scoreAction = new ScoreAction(100);
+      link.addAction(goldAction);
+      link.addAction(scoreAction);
+      ArrayList<Action> actionList = new ArrayList<>();
+      actionList.add(goldAction);
+      actionList.add(scoreAction);
+      assertEquals(actionList, link.getActions());
+    }
+
+    @Test
+    void getActionsWithEmptyActionList() {
+      assertTrue(link.getActions().isEmpty());
+    }
+
+    @Test
+    void getText() {
+      assertEquals("Text", link.getText());
+    }
+
   }
 
-  @Test
-  void addTwoActions() {
-    GoldAction goldAction = new GoldAction(500);
-    ScoreAction scoreAction = new ScoreAction(100);
-    link.addAction(goldAction);
-    link.addAction(scoreAction);
-    assertEquals(goldAction, link.getActions().get(0));
-    assertEquals(scoreAction, link.getActions().get(1));
+  @Nested
+  class LinkAdders {
+    @Test
+    void addTwoActions() {
+      GoldAction goldAction = new GoldAction(500);
+      ScoreAction scoreAction = new ScoreAction(100);
+      link.addAction(goldAction);
+      link.addAction(scoreAction);
+      assertEquals(goldAction, link.getActions().get(0));
+      assertEquals(scoreAction, link.getActions().get(1));
+    }
   }
 
-  @Test
-  void getActionsWithTwoActions() {
-    GoldAction goldAction = new GoldAction(100);
-    ScoreAction scoreAction = new ScoreAction(100);
-    link.addAction(goldAction);
-    link.addAction(scoreAction);
-    ArrayList<Action> actionList = new ArrayList<>();
-    actionList.add(goldAction);
-    actionList.add(scoreAction);
-    assertEquals(actionList, link.getActions());
-  }
+  @Nested
+  class LinkExceptionHandling {
+    @Test
+    void createLinkWithNullText() {
+      assertThrows(IllegalArgumentException.class, () -> new Link(null, "Test"));
+    }
 
-  @Test
-  void getActionsWithEmptyActionList() {
-    assertTrue(link.getActions().isEmpty());
-  }
-
-  @Test
-  void getText() {
-    assertEquals("Text", link.getText());
+    @Test
+    void createLinkWithNullReference() {
+      assertThrows(IllegalArgumentException.class, () -> new Link("Test", null));
+    }
   }
 }
--- src/test/java/edu/ntnu/idatt2001/PassageTest.java
+++ src/test/java/edu/ntnu/idatt2001/PassageTest.java
@@ -1,7 +1,11 @@
 package edu.ntnu.idatt2001;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,43 +17,83 @@ class PassageTest {
     passage = new Passage("Title", "Content");
   }
 
-  @Test
-  void getTitle() {
-    assertEquals("Title", passage.getTitle());
+  @Nested
+  class PassageGetters {
+
+    @Test
+    void getTitle() {
+      assertEquals("Title", passage.getTitle());
+    }
+
+    @Test
+    void getContent() {
+      assertEquals("Content", passage.getContent());
+    }
+
+    @Test
+    void getLinksWithEmptyList() {
+      assertTrue(passage.getLinks().isEmpty());
+    }
+
+    @Test
+    void getLinksWithTwoLinks() {
+      Link link1 = new Link("Test", "Test1");
+      Link link2 = new Link("Test", "Test2");
+      passage.addLink(link1);
+      passage.addLink(link2);
+      List<Link> list = new ArrayList<>();
+      list.add(link1);
+      list.add(link2);
+
+      assertEquals(list, passage.getLinks());
+    }
+
+    @Test
+    void hasLinksWithNoLinks() {
+      assertFalse(passage.hasLinks());
+    }
+
+    @Test
+    void hasLinksWithLinks() {
+      passage.addLink(new Link("t", "t"));
+      assertTrue(passage.hasLinks());
+    }
   }
 
-  @Test
-  void getContent() {
-    assertEquals("Content", passage.getContent());
+
+  @Nested
+  class PassageAdders {
+    @Test
+    void addLinkNotDuplicate() {
+      Link link = new Link("test", "test");
+      assertTrue(passage.addLink(link));
+      assertEquals(link, passage.getLinks().get(0));
+    }
+
+    @Test
+    void addLinkWhenDuplicate() {
+      Link link = new Link("T", "T");
+      passage.addLink(link);
+      assertFalse(passage.addLink(new Link("T", "T")));
+    }
   }
 
-  @Test
-  void getLinksWithEmptyList() {
-    assertTrue(passage.getLinks().isEmpty());
-  }
 
-  @Test
-  void hasLinksWithNoLinks() {
-    assertFalse(passage.hasLinks());
-  }
+  @Nested
+  class ExceptionHandling {
+    @Test
+    void createPassageWithNullTitle() {
+      assertThrows(IllegalArgumentException.class, () -> new Passage(null, "test"));
+    }
 
-  @Test
-  void addLinkNotDuplicate() {
-    Link link = new Link("test", "test");
-    assertTrue(passage.addLink(link));
-    assertEquals(link, passage.getLinks().get(0));
-  }
+    @Test
+    void createPassageWithNullContent() {
+      assertThrows(IllegalArgumentException.class, () -> new Passage("Test", null));
+    }
 
-  @Test
-  void addLinkWhenDuplicate() {
-    Link link = new Link("T", "T");
-    passage.addLink(link);
-    assertFalse(passage.addLink(link));
-  }
-
-  @Test
-  void hasLinksWithLinks() {
-    passage.addLink(new Link("t", "t"));
-    assertTrue(passage.hasLinks());
+    @Test
+    void addNullLink() {
+      assertThrows(NullPointerException.class, () -> passage.addLink(null));
+    }
   }
 }

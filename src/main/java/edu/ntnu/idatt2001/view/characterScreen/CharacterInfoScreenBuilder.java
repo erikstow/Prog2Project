3b704997f0aac9a256db130b879d47baeda628ff
erikstow <113420560+erikstow@users.noreply.github.com>
2023-05-16 11:50:24 +0200

    feat(GUI): :sparkles: Implement characterScreen view
    Implemented using Builder<Region> interface and created Widgets class to hold commonly used createFucntions for the builders.

---
+++ src/main/java/edu/ntnu/idatt2001/view/characterScreen/CharacterInfoScreenBuilder.java
@@ -0,0 +1,33 @@
+package edu.ntnu.idatt2001.view.characterScreen;
+
+import edu.ntnu.idatt2001.model.gui.CharacterScreenModel;
+import edu.ntnu.idatt2001.util.widgets.Widgets;
+import javafx.beans.property.StringProperty;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.util.Builder;
+
+public class CharacterInfoScreenBuilder implements Builder<Region> {
+
+  private final CharacterScreenModel model;
+
+  public CharacterInfoScreenBuilder(CharacterScreenModel model) {
+    this.model = model;
+  }
+
+  @Override
+  public Region build() {
+    VBox results = new VBox();
+    Node name = Widgets.createLabel("Who are you?", "character-info-label");
+    Node nameField = Widgets.createTextField("Name", model.name(), "character-info-text-field");
+    Node appearance = Widgets.createLabel("What do you look like?", "character-info-label");
+    Node appearanceField = Widgets.createTextField("Appearance", model.appearence(),
+        "character-info-text-field");
+    results.getChildren().addAll(name, nameField, appearance, appearanceField);
+    return results;
+  }
+}

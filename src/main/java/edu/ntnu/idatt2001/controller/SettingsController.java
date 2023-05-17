7d51f244d298f3628482b0f4eac6c330b7c458e6
Torgeir Haukaas <toha12744@gmail.com>
2023-05-17 11:28:46 +0200

    feat(gui): Create settings screen

---
+++ src/main/java/edu/ntnu/idatt2001/controller/SettingsController.java
@@ -0,0 +1,38 @@
+package edu.ntnu.idatt2001.controller;
+
+import edu.ntnu.idatt2001.model.events.ControllerEvent;
+import edu.ntnu.idatt2001.model.events.DataUpdateEvent;
+import edu.ntnu.idatt2001.model.gui.SettingsModel;
+import edu.ntnu.idatt2001.view.SettingsScreenBuilder;
+import javafx.scene.layout.Region;
+
+public class SettingsController extends Controller {
+  private final Region view;
+  private final SettingsModel model;
+
+  SettingsController() {
+    model = new SettingsModel();
+    this.view = new SettingsScreenBuilder(model, this::resume, this::exitToTitle, this::restartGame).build();
+  }
+
+  private void resume() {
+    update(new DataUpdateEvent("resumeGame", null));
+  }
+
+  private void restartGame() {
+    update(new DataUpdateEvent("restartGame", null));
+  }
+
+  private void exitToTitle() {
+    update(new DataUpdateEvent("returnToTitle", null));
+  }
+
+  @Override
+  public void onUpdate(ControllerEvent event) {
+    //
+  }
+
+  public Region getView() {
+    return this.view;
+  }
+}

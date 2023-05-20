f217b4d7c108a4066d73bdcf093153893e668b19
Torgeir Haukaas <toha12744@gmail.com>
2023-05-20 10:45:26 +0200

    fix(gui): Fix input validation in goal adder

--- src/main/java/edu/ntnu/idatt2001/view/characterScreen/CharacterGoalsScreenBuilder.java
+++ src/main/java/edu/ntnu/idatt2001/view/characterScreen/CharacterGoalsScreenBuilder.java
@@ -4,22 +4,16 @@ import edu.ntnu.idatt2001.model.goals.Goal;
 import edu.ntnu.idatt2001.model.gui.characterScreenModel.CharacterScreenModel;
 import edu.ntnu.idatt2001.util.widgets.Widgets;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.util.Builder;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 
 public class CharacterGoalsScreenBuilder implements Builder<Region> {
-  
+
   private final CharacterScreenModel model;
   private final Runnable addGoal;
   private final Runnable undoGoal;
@@ -53,25 +47,53 @@ public class CharacterGoalsScreenBuilder implements Builder<Region> {
   private Node createGoalAdderBox() {
     VBox results = new VBox();
     Label goalAddPrompt = Widgets.createLabel("Choose goals to add", "");
-    TextField goalValue = new TextField();
-    goalValue.textProperty().bindBidirectional(model.goalValue());
-    ComboBox<String> goalType = new ComboBox<>();
-    goalType.getItems().addAll("Health", "Gold", "Score", "Inventory");
-    goalType.valueProperty().bindBidirectional(model.goalType());
-    goalType.valueProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue.equals("Health")) {
-        goalValue.setPromptText("An integer...");
-      } else if (newValue.equals("Gold")) {
-        goalValue.setPromptText("An integer...");
-      } else if (newValue.equals("Score")) {
-        goalValue.setPromptText("An integer...");
-      } else if (newValue.equals("Inventory")) {
-        goalValue.setPromptText("Comma-separated list of items...");
-      }
-    });
+    ComboBox<String> goalType = createGoalTypeComboBox();
+    TextField goalValue = createGoalValueField(goalType);
+
     Button addGoalButton = Widgets.createButton("Add Goal", addGoal, "add-goal-button");
+    addGoalButton.disableProperty().bind(goalValue.textProperty().isEmpty().or(goalType.valueProperty().isNull()));
     results.getChildren().addAll(goalAddPrompt, goalType, goalValue, addGoalButton);
     return results;
   }
-}
 
+  private TextField createGoalValueField(ComboBox<String> goalType) {
+    TextField results = new TextField();
+    results.textProperty().bindBidirectional(model.goalValue());
+    results.disableProperty().bind(goalType.valueProperty().isNull());
+
+    goalType.valueProperty().addListener((observable, oldValue, newValue) -> {
+      results.setText("");
+      if (newValue.equals("Inventory")) {
+        results.setPromptText("Comma-separated list of items...");
+        results.setTextFormatter(null);
+      } else {
+        results.setPromptText("An integer...");
+        results.setTextFormatter(getPositiveIntegerFormatter());
+      }
+    });
+
+    return results;
+  }
+
+  private ComboBox<String> createGoalTypeComboBox() {
+    ComboBox<String> results = new ComboBox<>();
+
+    results.setPromptText("Choose goal type");
+    results.getItems().addAll("Health", "Gold", "Score", "Inventory");
+    results.valueProperty().bindBidirectional(model.goalType());
+
+    return results;
+  }
+
+  private TextFormatter<String> getPositiveIntegerFormatter() {
+    UnaryOperator<TextFormatter.Change> filter = change -> {
+      String newText = change.getControlNewText();
+      if (Pattern.matches("\\d*", newText)) {
+        return change;
+      }
+      return null;
+    };
+    return new TextFormatter<>(filter);
+  }
+
+}
